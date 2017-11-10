5e03ed31e2009148c09e0afa6ba1048e94da83c0
ws19921108 <32241390+ws19921108@users.noreply.github.com>
2017-11-10 11:09:06 +0800

    1110

--- Money.py
+++ Money.py
@@ -5,13 +5,18 @@ sys.setdefaultencoding('utf-8')
 
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-# from pyecharts import Scatter3D
-# from pyecharts.constants import DEFAULT_HOST
+from pyecharts import Bar
+from pyecharts.constants import DEFAULT_HOST
 import urllib2
 import json
 import psycopg2
 from datetime import date
 from bs4 import BeautifulSoup
+
+MONEY = 1000
+RATE = 0.9985
+WEEKDAYS = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+symbol_list = []
 
 
 app = Flask(__name__)
@@ -98,6 +103,90 @@ def get_finance_top_ntes():
     return ntes_list[:20]
 
 
+def StoreFundData(symbol):
+
+    sql = "SELECT tablename FROM pg_tables WHERE schemaname='public' AND tablename='t%s'" % symbol
+    cur.execute(sql)
+    res = cur.fetchone()
+    if res:
+        sql = "DROP TABLE t%s" % symbol
+        cur.execute(sql)
+    sql = "CREATE TABLE t%s (id serial PRIMARY KEY, tdate date, value real)" \
+          % symbol
+    cur.execute(sql)
+
+    page = 1
+    total_num = -1
+    while True:
+        url = 'http://stock.finance.sina.com.cn/fundInfo/api/openapi.php/CaihuiFundInfoService.getNav?symbol=' + symbol + '&page=' + str(
+            page)
+        request = urllib2.Request(url=url)
+        response = urllib2.urlopen(request, timeout=20)
+        result = response.read()
+        result = json.loads(result)['result']
+        if total_num < 0:
+            total_num = int(result['data']['total_num'])
+            # print total_num
+        data = result['data']['data']
+        if len(data) > 20:
+            data.pop(-1)
+        for item in data:
+            tdate = item['fbrq'].split()[0]
+            value = item['jjjz']
+            sql = "INSERT INTO t%s (tdate, value) VALUES ('%s', %s)" % (symbol, tdate, value)
+            # print sql
+            cur.execute(sql)
+        if page * 20 > total_num:
+            break
+        page += 1
+    conn.commit()
+
+def GetFundData(symbol):
+    sql = "SELECT tdate,value FROM t%s ORDER BY tdate DESC LIMIT 500" % symbol
+    cur.execute(sql)
+    rows = cur.fetchall()
+    return rows
+
+def GetLatestPrice(symbol):
+    sql = "SELECT value FROM t%s WHERE id=1" % symbol
+    cur.execute(sql)
+    rows = cur.fetchone()
+    return rows[0]
+
+def OrderFundByWeekday(symbol,weekday):
+    count = 0
+    fund_num = 0
+    latest = GetFundData(symbol)
+    for item in latest:
+        if weekday == item[0].weekday():
+            count += 1
+            fund_num += MONEY * RATE / item[1]
+    return count, fund_num
+
+def CalcFundByWeek(symbol):
+    week_list = []
+    latest_price = GetLatestPrice(symbol)
+    for weekday in range(5):
+        count, fund_num = OrderFundByWeekday(symbol,weekday)
+        money_sum = count * MONEY
+        money_value = latest_price * fund_num
+        money_value = round(money_value, 2)
+        return_rate = (money_value-money_sum)/money_sum
+        return_rate = round(return_rate,4)
+        week_list.append((WEEKDAYS[weekday], money_sum,money_value,return_rate))
+    return week_list
+
+def eBar(week_list):
+    attrs = []
+    values = []
+    for item in week_list:
+        attrs.append(item[0])
+        values.append(item[3])
+    bar = Bar("定投回报")
+    bar.add("回报率", attrs, values, mark_point=["max", "min"])
+    return bar
+
+
 @app.route('/')
 def News():
     sina_list = get_finance_top_sina()
@@ -111,6 +200,7 @@ def TopBill():
     bill_list = get_top_bill_symbol(table_name)
     # print bill_list
     return render_template('top_bill.html', bill_list=bill_list)
+
 @app.route('/tb/<symbol>')
 def ShowSymbol(symbol):
     table_name = 'top_bill' + day
@@ -119,9 +209,25 @@ def ShowSymbol(symbol):
 
 @app.route('/fund', methods=['GET', 'POST'])
 def Fund():
+    week_list = []
+    symbol = ''
     if request.method == 'POST':
-        print request.form['symbol']
-    return render_template('fund.html')
+        symbol = request.form['symbol']
+    elif 'symbol' in request.args:
+        symbol = request.args['symbol']
+    if symbol != '':
+        if symbol not in symbol_list:
+            StoreFundData(symbol)
+            symbol_list.append(symbol)
+        week_list = CalcFundByWeek(symbol)
+    bar = eBar(week_list)
+    return render_template('fund.html',
+                           symbol=symbol,
+                           week_list=week_list,
+                           symbol_list=symbol_list,
+                           bar=bar.render_embed(),
+                           host=DEFAULT_HOST,
+                           script_list=['echarts.min'])
 
 if __name__ == '__main__':
     app.run()
